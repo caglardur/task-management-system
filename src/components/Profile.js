541d44aa040c1c383ae9842cf1ef08042999c70b
caglar.durmus <caglardur@gmail.com>
2021-12-19 23:33:01 +0300

    add "mytask" ling to profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
+import { Link } from "react-router-dom"
 
 import { removeUser } from "../redux/UserReducer"
 
@@ -26,27 +27,29 @@ const Profile = () => {
   return (
     <div className="col">
       {userDetail && (
-        <div className="row align-items-center">
-          <div className="col-auto">
-            <i className="bi bi-person-circle fs-1"></i>
-          </div>
-          <div className="col-auto">
+        <Link to="mytasks">
+          <div className="row align-items-center">
             <div className="col-auto">
-              Hi, {userDetail.name}
-              <i
-                type="button"
-                className="bi bi-box-arrow-right ms-2 text-danger"
-                onClick={() => {
-                  localStorage.removeItem("userData")
-                  dispatch(removeUser())
-                }}
-              ></i>
+              <i className="bi bi-person-circle fs-1"></i>
             </div>
-            <div className="col-auto" style={{ fontSize: "14px" }}>
-              {userDetail.title}
+            <div className="col-auto">
+              <div className="col-auto">
+                Hi, {userDetail.name}
+                <i
+                  type="button"
+                  className="bi bi-box-arrow-right ms-2 text-danger"
+                  onClick={() => {
+                    localStorage.removeItem("userData")
+                    dispatch(removeUser())
+                  }}
+                ></i>
+              </div>
+              <div className="col-auto" style={{ fontSize: "14px" }}>
+                {userDetail.title}
+              </div>
             </div>
           </div>
-        </div>
+        </Link>
       )}
     </div>
   )
